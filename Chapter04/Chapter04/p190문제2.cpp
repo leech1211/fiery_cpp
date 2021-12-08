8d267c8c9a303875dd61c98f11ae1bfdfc154e43
leech1211 <leech1211@nate.com>
2021-12-08 14:04:29 +0900

    Chapter 04

--- Chapter04/Chapter04/p190문제2.cpp
+++ Chapter04/Chapter04/p190문제2.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
+
+namespace COMP_POS
+{
+	enum
+	{
+		CLERK, SENIOR, ASSIST, MANAGER
+	};
+}
 
 class NameCard
 {
 private:
-	char name;
-	char company;
-	char call;
-	int COMP_POS
-	enum 
-	{
-		CLERK,SENIOR,ASSIST,MANAGER
-	};
+	char* name;
+	char* company;
+	char* call;
+	int COMP_POS;
 
 public:
-	NameCard(char* name, char* company,char* call, int COMP_POS)
+	NameCard(char* namep, char* companyp,char* callp, int COMP_POSp)	//생성자
 	{
-
+		name = strcpy(namep, name);
+		company = strcpy(companyp, company);
+		call = strcpy(callp, call);
+		COMP_POS = COMP_POSp;
 	}
 
 	void ShowNameCardInfo() const
@@ -27,7 +35,7 @@ public:
 		cout << "직급: " << COMP_POS << endl;
 
 	}
-	~NameCard()
+	~NameCard()		//소멸자
 	{
 
 	}
@@ -40,10 +48,13 @@ int main()
 {
 	NameCard manClerk("Lee", "ABCEng", "010-1111-2222", COMP_POS::CLERK);
 	NameCard manSENIOR("Hong", "OrangrEng", "010-3333-4444", COMP_POS::SENIOR);
-	NameCard manAssist("Kim", "SoGoodComp", "010-5555-6666", COMP_POS::Assist);
+	NameCard manAssist("Kim", "SoGoodComp", "010-5555-6666", COMP_POS::ASSIST);
 
 	manClerk.ShowNameCardInfo();
 	manSENIOR.ShowNameCardInfo();
 	manAssist.ShowNameCardInfo();
 	return 0;
 }
+
+
+//이거 나중에 다시 보기로
